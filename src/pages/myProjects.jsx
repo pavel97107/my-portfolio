71b4946baa315c43a5b0b342f8605fb20ebdbfb3
pavel97107 <pavel97107@gmail.com>
2020-12-04 00:00:59 +0300

    stylingOurProjects

--- src/pages/myProjects.jsx
+++ src/pages/myProjects.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import netflix from "../img/netflix.png";
+import multiform from "../img/multi.png";
+import musicapp from "../img/waves.png";
+
+//styledComponent
+import { Projects, Project } from "./styles";
 
 export default () => {
   return (
-    <div>
-      <h1>My projects</h1>
-    </div>
+    <Projects>
+      <Project>
+        <h2>Netflix Clone</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={netflix} alt="neflix-clone" />
+        </Link>
+      </Project>
+      <Project>
+        <h2>Multi-Step Register Form</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={multiform} alt="multi-step-form" />
+        </Link>
+      </Project>
+      <Project>
+        <h2>Music App</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={musicapp} alt="music-app" />
+        </Link>
+      </Project>
+    </Projects>
   );
 };
---
+++ src/pages/styles/index.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+
+
+
+export const Projects = styled.div`
+    min-height: 100vh;
+    overflow: hidden;
+    padding: 5rem 10rem;
+
+    h2 {
+        padding: 1rem 0rem;
+    }
+
+`
+export const Project = styled.div`
+    padding-bottom: 10rem;
+
+    .line {
+        height: 0.5rem;
+        background: #ccc;
+        margin-bottom: 3rem;
+    }
+    img {
+        width: 100%;
+        height: 70vh;
+        object-fit: cover;
+    }
+
+    
+`
