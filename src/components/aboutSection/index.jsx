77724d60f7e7eee11ad909548bf18bd7743617a7
pavel97107 <pavel97107@gmail.com>
2020-12-05 00:43:38 +0300

    useFramerMotion

--- src/components/aboutSection/index.jsx
+++ src/components/aboutSection/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import myPhoto from "../../img/me.jpg";
+import { motion } from "framer-motion";
 
 //StyledComponents
 import {
@@ -16,27 +17,40 @@ import {
 } from "./styles/aboutSection";
 
 export default () => {
+  const titleAnim = {
+    hidden: { opacity: 0, transition: { duration: 2 } },
+    show: { opacity: 1 },
+  };
+
+  const container = {
+    hidden: { x: 100 },
+    show: {
+      x: 0,
+      transition: { duration: 1, ease: "easeOut", staggerChildren: 1 },
+    },
+  };
+
   return (
     <About>
       <Description>
-        <Title>
+        <motion.div variants={container} initial="hidden" animate="show">
           <Hide>
-            <Text>Hi</Text>
+            <motion.h2 variants={titleAnim}>Hi</motion.h2>
           </Hide>
           <Hide>
-            <Text>
+            <motion.h2 variants={titleAnim}>
               My name is <CustomizeText>Pavel</CustomizeText>
-            </Text>
+            </motion.h2>
           </Hide>
-          <Hide>
-            <Text>I'am Frontend Developer</Text>
-          </Hide>
-          <InfoText>
-            Contact me if you need a promising developer with a desire to
-            constantly develop.
-          </InfoText>
-          <Button>CV</Button>
-        </Title>
+          <motion.h2 variants={titleAnim}>
+            <Text>I'am Frontend Developer.</Text>
+          </motion.h2>
+        </motion.div>
+        <InfoText>
+          Contact me if you need a promising developer with a desire to
+          constantly develop.
+        </InfoText>
+        <Button>CV</Button>
       </Description>
       <ImageContainer>
         <Image src={myPhoto} alt="myPhoto" />
