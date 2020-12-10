d780d0f8055641b5fbedf42e2366827df4919ada
pavel97107 <pavel97107@gmail.com>
2020-12-10 14:18:17 +0300

    production_v1

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as AboutSection } from "./aboutSection";
-export { default as SkillsSection } from "./skillsSection";
-export { default as FaqSection} from "./faqSection";
-export {default as Nav} from './nav'
-export {default as Wave} from './wave'
--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as AboutMe } from "./aboutMe";
-export { default as ContactMe } from "./contactMe";
-export { default as MyProjects } from "./myProjects";
-export { default as ProjectDetail } from "./projectDetail";
